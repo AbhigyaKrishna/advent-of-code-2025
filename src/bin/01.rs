12a0f9b3cd8d0216d8eb2713d1bd4ff627e036e8
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-12-30 01:27:43 +0530

    Day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,5 @@
 use advent_of_code::MinHeap;
+use std::collections::HashMap;
 
 advent_of_code::solution!(1);
 
@@ -29,7 +30,27 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut sum = 0u64;
+
+    let mut left_count = HashMap::<u32, usize>::new();
+    let mut right_count = HashMap::<u32, usize>::new();
+    for line in input.lines() {
+        let mut split = line.split("   ");
+        let num1 = split.next()?.parse::<u32>().unwrap();
+        let num2 = split.next()?.parse::<u32>().unwrap();
+
+        sum += (*left_count.get(&num2).unwrap_or(&0) * num2 as usize) as u64;
+        sum += (*right_count.get(&num1).unwrap_or(&0) * num1 as usize) as u64;
+
+        if num1 == num2 {
+            sum += num1 as u64;
+        }
+
+        left_count.entry(num1).and_modify(|e| *e += 1).or_insert(1);
+        right_count.entry(num2).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -45,6 +66,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
